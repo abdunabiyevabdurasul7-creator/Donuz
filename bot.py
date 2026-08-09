1773edf0cac2da353f7bc597a61a54ad556b523b
abdunabiyevabdurasul7-creator <abdunabiyevabdurasul7@gmail.com>
2026-08-09 16:07:00 +0500

    Change bot token in bot.py
    Updated the bot token for authentication.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram.ext import (
 )
 
 # --- SOZLAMALAR ---
-BOT_TOKEN = "8659066280:AAHVBesvBvkTueTi4LBcLSarBWFKJYWP8FI"
+BOT_TOKEN = "8659066280:AAE9Dwoon0tkHVRXR7ojyc4Bmr07nSibauA"
 ADMIN_ID = 5692925792  # Admin Telegram ID
 
 # Database Sozlash
